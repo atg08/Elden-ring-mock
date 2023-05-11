12932b1b77adaca0b477d5666420981174b6f427
AdittiGupta <agup0031@student.monash.edu>
2023-05-11 15:52:13 +0800

    implemented REQ3 Astrologer

---
+++ src/game/gameactors/players/Astrologer.java
@@ -0,0 +1,13 @@
+package game.gameactors.players;
+
+import game.weapons.Uchigatana;
+
+public class Astrologer extends Player{
+    /**
+     * Constructs a Player object with the specified number of hitpoints.
+     */
+    public Astrologer() {
+        super(396);
+        addWeaponToInventory(new Uchigatana());
+    }
+}
