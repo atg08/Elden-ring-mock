94cf01d6cf17a8e20e6edf0638a541e3331f6ffc
AdittiGupta <agup0031@student.monash.edu>
2023-05-15 03:05:09 +0800

    - implemented - player dies if stepped on FlareOfInferno

--- src/game/environments/FlareOfInferno.java
+++ src/game/environments/FlareOfInferno.java
@@ -1,7 +1,11 @@
 package game.environments;
 
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
+import game.actions.DeathAction;
+import game.gameactors.StatusActor;
 import game.gameactors.enemies.Enemy;
 import game.gameactors.enemies.fire.BlackflameMonk;
 import game.gameactors.enemies.fire.FireMonk;
@@ -48,5 +52,15 @@ public class FlareOfInferno extends Environment{
 
         }
         return null;
+    }
+
+
+    public void tick(Location location) {
+        // if player -> trigger death action
+        Actor actor = location.getActor();
+        if (actor != null && actor.hasCapability(StatusActor.IS_PLAYER)){
+            String result = new DeathAction(actor).execute(actor, location.map());
+            new Display().println(result);
+        }
     }
 }
