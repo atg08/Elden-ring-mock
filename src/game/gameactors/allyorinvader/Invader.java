1a9b593282cb3e07226fd28f2478db9a719001eb
tanul <tanulgupta3@gmail.com>
2023-05-15 17:07:21 +0800

    Made small changes to the Invader class

--- src/game/gameactors/allyorinvader/Invader.java
+++ src/game/gameactors/allyorinvader/Invader.java
@@ -3,17 +3,23 @@ package game.gameactors.allyorinvader;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.actions.DespawnAction;
+import game.gameactors.StatusActor;
 import game.gameactors.enemies.Enemy;
 import game.gameactors.players.Player;
+import game.reset.ResetManager;
 import game.reset.Resettable;
 
 public class Invader extends Enemy implements Resettable {
+
+    private ResetManager rm = ResetManager.getInstance();
     /**
      * Constructor for the Enemy class.
      */
     public Invader(Player player) {
         super("invader", 'ඞ', player.getMaxHP(), 1358, 5578);
+        this.addCapability(StatusActor.IS_INVADER);
         addWeaponToInventory(player.getWeaponInventory().get(0));
+        rm.registerResettable(this);
     }
 
     @Override
